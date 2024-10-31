6ed00ea92bd1f2aba19e40698ed74dd323259c9e
Ali <adadashipour@gmail.com>
2024-10-31 13:28:20 +0000

    Add attributes and constructors

---
+++ ThAmCo.Catering/Data/FoodBooking.cs
@@ -0,0 +1,21 @@
+﻿namespace ThAmCo.Catering.Data
+{
+    public class FoodBooking
+    {
+        public int FoodItemId { get; set; }
+        public string Description { get; set; }
+        public float UnitPrice { get; set; }
+
+        public FoodBooking()
+        {
+            Description = string.Empty;
+        }
+
+        public FoodBooking(int foodItemId, string description, int unitPrice)
+        {
+            FoodItemId = foodItemId;
+            Description = description;
+            UnitPrice = unitPrice;
+        }
+    }
+}
