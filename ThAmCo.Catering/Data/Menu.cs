cd3daf65ad168a090ca001c843e7bcc0f500cac6
Ali <adadashipour@gmail.com>
2024-10-31 13:27:43 +0000

    Add Menu attributes and constructors

---
+++ ThAmCo.Catering/Data/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThAmCo.Catering.Data
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        [Required]
+        public string MenuName { get; set; }
+
+        public Menu()
+        {
+            MenuName = string.Empty;
+        }
+
+        public Menu(int menuId, string menuName)
+        {
+            MenuId = menuId;
+            MenuName = menuName;
+        }
+    }
+}
