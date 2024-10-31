a72d04ff9955dd276b2b91a23d289a2f87b1a547
Ali <adadashipour@gmail.com>
2024-10-31 14:23:12 +0000

    Add attributes and constractor for MenuFoodItems

---
+++ ThAmCo.Catering/Data/MenuFoodItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ThAmCo.Catering.Data
+{
+    public class MenuFoodItem
+    {
+        public int MenuId { get; set; }
+        public int FoodItemId { get; set; }
+
+        public MenuFoodItem()
+        {
+
+        }
+
+        public MenuFoodItem(int menuId, int foodItemId)
+        {
+            MenuId = menuId;
+            FoodItemId = foodItemId;
+        }
+    }
+}
