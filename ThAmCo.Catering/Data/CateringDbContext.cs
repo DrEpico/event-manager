8b28a725bf76a228a869482214766727d2e1d43f
Ali <adadashipour@gmail.com>
2024-10-31 14:23:47 +0000

    Add Catering DbContext

---
+++ ThAmCo.Catering/Data/CateringDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ThAmCo.Catering.Data
+{
+    public class CateringDbContext : DbContext
+    {
+        public string DbPath { get; }
+
+        public CateringDbContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "catering.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
