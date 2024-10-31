bedbdecdcee989e8f5f5df952827dc571491ebcf
Ali <adadashipour@gmail.com>
2024-10-31 13:36:33 +0000

    Move the attributes and constructors from FoodBookins to the correct file (FoodItem)

---
+++ ThAmCo.Catering/Data/FoodItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ThAmCo.Catering.Data
+{
+    public class FoodItem
+    {
+            public int FoodItemId { get; set; }
+            public string Description { get; set; }
+            public float UnitPrice { get; set; }
+
+            public FoodItem()
+            {
+                Description = string.Empty;
+            }
+
+            public FoodItem(int foodItemId, string description, int unitPrice)
+            {
+                FoodItemId = foodItemId;
+                Description = description;
+                UnitPrice = unitPrice;
+            }
+        }
+}
