8638f8545b394aa4fdd44a9b8f38f360c642c161
Ali <adadashipour@gmail.com>
2024-11-13 21:10:12 +0000

    Tweak DTO validation

--- ThAmCo.Catering/Dtos/FoodItemDto.cs
+++ ThAmCo.Catering/Dtos/FoodItemDto.cs
@@ -5,10 +5,9 @@ namespace ThAmCo.Catering.Dtos
     public class FoodItemDto
     {
         [Required]
-        [StringLength(100, MinimumLength = 3)]
+        [StringLength(50, MinimumLength = 3)]
         public string Description { get; set; } = string.Empty;
         [Range(0.25, 100)]
         public float UnitPrice { get; set; }
-
     }
 }
