7b364577c69d161f72cde41dccb70c762dab4121
Ali <adadashipour@gmail.com>
2024-11-14 15:52:27 +0000

    Make the fields nullable

--- ThAmCo.Catering/Dtos/FoodItemDto.cs
+++ ThAmCo.Catering/Dtos/FoodItemDto.cs
@@ -5,8 +5,8 @@ namespace ThAmCo.Catering.Dtos
     public class FoodItemDto
     {
         [StringLength(50, MinimumLength = 3)]
-        public string Description { get; set; } = string.Empty;
-        [Range(0.25, 100)]
-        public float UnitPrice { get; set; }
+        public string? Description { get; set; } = string.Empty;
+        [Range(0.25, 100, ErrorMessage = "Above £100 for a dish is crazy.")]
+        public float? UnitPrice { get; set; }
     }
 }
