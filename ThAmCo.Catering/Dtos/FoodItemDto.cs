143c4fbebe907749f53fcec6f91c932e5e166a27
Ali <adadashipour@gmail.com>
2024-11-14 12:57:37 +0000

    Remove the required validation from the DooItemDto to allow only one of the fields to be updated

--- ThAmCo.Catering/Dtos/FoodItemDto.cs
+++ ThAmCo.Catering/Dtos/FoodItemDto.cs
@@ -4,7 +4,6 @@ namespace ThAmCo.Catering.Dtos
 {
     public class FoodItemDto
     {
-        [Required]
         [StringLength(50, MinimumLength = 3)]
         public string Description { get; set; } = string.Empty;
         [Range(0.25, 100)]
