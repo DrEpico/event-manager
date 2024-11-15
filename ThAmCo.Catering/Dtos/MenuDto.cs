375871c6090e214ef128d37fc651abff04a1367e
Ali <adadashipour@gmail.com>
2024-11-15 15:33:08 +0000

    Add another class to the Menu DTO and separate them by input or output.

--- ThAmCo.Catering/Dtos/MenuDto.cs
+++ ThAmCo.Catering/Dtos/MenuDto.cs
@@ -2,9 +2,21 @@
 
 namespace ThAmCo.Catering.Dtos
 {
-    public class MenuDto
+    public class MenuInputDto
     {
         [StringLength(50, MinimumLength = 3)]
         public string MenuName { get; set; } = string.Empty;
+    }
+
+    public class MenuOutputDto
+    {
+        public int MenuId { get; set; } 
+        public string MenuName { get; set; } = string.Empty;
+
+        public MenuOutputDto(int menuId, string menuName)
+        {
+            MenuId = menuId;
+            MenuName = menuName;
+        }
     }
 }
