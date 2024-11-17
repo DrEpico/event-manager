a04020488cc47d2f67f0fca1d5b4fdf97915e618
Ali <adadashipour@gmail.com>
2024-11-17 19:50:54 +0000

    Add new constructor

--- ThAmCo.Catering/Dtos/FoodItemDto.cs
+++ ThAmCo.Catering/Dtos/FoodItemDto.cs
@@ -4,9 +4,11 @@ namespace ThAmCo.Catering.Dtos
 {
     public class FoodItemDto
     {
-        [StringLength(50, MinimumLength = 3)]
+        [StringLength(50)]
         public string? Description { get; set; } = string.Empty;
-        [Range(0.25, 100, ErrorMessage = "Above £100 for a dish is crazy.")]
+
+        //Range is 0-100 to avoid negative values for the float.
+        [Range(0, 100, ErrorMessage = "Above £100 for a dish is crazy.")]
         public float? UnitPrice { get; set; }
     }
 }
