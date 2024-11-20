673271d7fc49e1a7d13ad93ec59619dbbecd213e
Ali <adadashipour@gmail.com>
2024-11-20 23:34:26 +0000

    Add Staff object attributes

--- ThAmCo.Events/Data/Staff.cs
+++ ThAmCo.Events/Data/Staff.cs
@@ -2,5 +2,11 @@
 {
     public class Staff
     {
+        public int StaffId { get; set; } 
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        // Navigation Properties
+        public List<Staffing> Staffings { get; set; } = new();
     }
 }
