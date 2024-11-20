43143a1d0670da962f7f5a0ef87ebab74a5d4646
Ali <adadashipour@gmail.com>
2024-11-20 23:07:38 +0000

    Add Guest object attributes

--- ThAmCo.Events/Data/Guest.cs
+++ ThAmCo.Events/Data/Guest.cs
@@ -2,5 +2,12 @@
 {
     public class Guest
     {
+        public int GuestId { get; set; } // Primary Key
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        // Navigation Properties
+        public List<GuestBooking> GuestBookings { get; set; } = new();
     }
 }
