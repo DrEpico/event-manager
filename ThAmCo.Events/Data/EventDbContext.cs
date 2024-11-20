19fbda2d0b9db537859ad99df1251ffbfc0ca54e
Ali <adadashipour@gmail.com>
2024-11-20 23:48:28 +0000

    Create EventDbContext with constructor

---
+++ ThAmCo.Events/Data/EventDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ThAmCo.Events.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<GuestBooking> GuestBookings { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public string DbPath { get; set; } = string.Empty;
+
+        public EventDbContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "ThAmCo.Event.db");
+        }
+    }
+}
