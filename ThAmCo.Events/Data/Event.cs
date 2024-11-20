09d124a404842cf149b078cd1c7c19f2068db313
Ali <adadashipour@gmail.com>
2024-11-20 23:27:15 +0000

    Add Event object attributes

--- ThAmCo.Events/Data/Event.cs
+++ ThAmCo.Events/Data/Event.cs
@@ -2,5 +2,13 @@
 {
     public class Event
     {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string EventType { get; set; }
+
+        // Navigation Properties
+        public List<GuestBooking> GuestBookings { get; set; } = new();
+        public List<Staffing> Staffings { get; set; } = new();
     }
 }
