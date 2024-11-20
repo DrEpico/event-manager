3c6ef356b91242ffea57d1f4d453fcbbcd586114
Ali <adadashipour@gmail.com>
2024-11-20 23:11:17 +0000

    Add GuestBooking object attributes

--- ThAmCo.Events/Data/GuestBooking.cs
+++ ThAmCo.Events/Data/GuestBooking.cs
@@ -2,5 +2,13 @@
 {
     public class GuestBooking
     {
+        public int GuestBookingId { get; set; }
+        public int GuestId { get; set; }
+        public int EventId { get; set; }
+        public bool HasAttended { get; set; }
+
+        // Navigation Properties
+        public List<GuestBooking> GuestBookings { get; set; } = new();
+        public List<Staffing> Staffings { get; set; } = new();
     }
 }
