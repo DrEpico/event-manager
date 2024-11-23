7173e128c171531e502bca05d33dc86ab87ba647
Ali <adadashipour@gmail.com>
2024-11-23 18:30:41 +0000

    Update OnGet() for EventList>Create.cshtml.cs to get a list of EventTypes to implement them in a dropdown list

--- ThAmCo.Events/Pages/EventList/Create.cshtml.cs
+++ ThAmCo.Events/Pages/EventList/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using ThAmCo.Events.Data;
 
 namespace ThAmCo.Events.Pages.EventList
@@ -13,18 +14,29 @@ namespace ThAmCo.Events.Pages.EventList
     {
         private readonly ThAmCo.Events.Data.EventDbContext _context;
 
+        //public List<string> EventTypes { get; set; }
+
         public CreateModel(ThAmCo.Events.Data.EventDbContext context)
         {
             _context = context;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            EventTypes = await _context.Events
+                .Select(e => e.EventType)
+                .Distinct()
+                .ToListAsync();
+
             return Page();
         }
 
+
         [BindProperty]
         public Event Event { get; set; } = default!;
+
+        // Dropdown options
+        public List<string> EventTypes { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
