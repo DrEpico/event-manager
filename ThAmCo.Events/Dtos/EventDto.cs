2f568f0df16a74e9ca9008c7018f0aa9518a1afe
Ali <adadashipour@gmail.com>
2024-11-29 17:54:21 +0000

    Update EventDto name to VenueAvailabilityDto

--- ThAmCo.Events/Dtos/EventDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ThAmCo.Events.Dtos
-{
-    public class EventDto
-    {
-        public required string Title;
-        public required DateTime Date;
-        public required string EventType;
-    }
-}
---
+++ ThAmCo.Events/Dtos/VenueAvailabilityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ThAmCo.Events.Dtos
+{
+    public class VenueAvailabilityDto
+    {
+        public required string Code;
+        public required string Name;
+        public required string Description;
+        public required int Capacity;
+        public required DateTime Date;
+        public required double CostPerHour;
+    }
+}
