10cf7ad37bcd9e6cff31cd99b5fb75c6f1b70dec
Ali <adadashipour@gmail.com>
2024-11-30 00:00:10 +0000

    Refactor VenueAvailability related code

---
+++ ThAmCo.Events/Services/VenueEventTypeService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace ThAmCo.Events.Services
+{
+    public class VenueEventTypeService
+    {
+        const string ServiceBaseUrl = "https://localhost:7088/api";
+        const string VenueEndpoint = "/EventType";
+        private readonly HttpClient _httpClient;
+
+        public VenueEventTypeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+    }
+}
