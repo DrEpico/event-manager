d97c459011f5c369dc8e6bca8a688b0050d87445
Ali <adadashipour@gmail.com>
2024-12-01 12:05:11 +0000

    Migration applying VenueCode property

---
+++ ThAmCo.Events/Data/Migrations/20241201115628_VenueCode.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThAmCo.Events.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VenueCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VenueCode",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 1,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 2,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 3,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 4,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 5,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 6,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 7,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 8,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 9,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 10,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 11,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 12,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 13,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 14,
+                column: "VenueCode",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: 15,
+                column: "VenueCode",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VenueCode",
+                table: "Events");
+        }
+    }
+}
