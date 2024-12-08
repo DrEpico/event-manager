6ab03574195cad9251421fbf9afb5dbe16bfaa63
Ali <adadashipour@gmail.com>
2024-12-08 21:03:58 +0000

    Add the method to handle new guest bookings

--- ThAmCo.Events/Pages/EventList/Details.cshtml.cs
+++ ThAmCo.Events/Pages/EventList/Details.cshtml.cs
@@ -43,5 +43,35 @@ namespace ThAmCo.Events.Pages.EventList
             }
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAddGuestAsync(string GuestName, string email, string phoneNum)
+        {
+            if (string.IsNullOrEmpty(GuestName))
+            {
+                ModelState.AddModelError(string.Empty, "Guest name is required.");
+                return Page();
+            }
+
+            // Assuming EventId is passed as a route parameter
+            var eventId = Event.EventId;
+
+            Guest guest = new Guest(GuestName, email, phoneNum);
+
+            _context.Guests.Add(guest);
+            await _context.SaveChangesAsync();
+
+            var newGuest = new GuestBooking
+            {
+                EventId = eventId,
+                GuestId = guest.GuestId,
+            };
+
+            _context.GuestBookings.Add(newGuest);
+            await _context.SaveChangesAsync();
+
+            //return RedirectToPage("./Details", new { id = eventId });
+            return Page();
+        }
+
     }
 }
