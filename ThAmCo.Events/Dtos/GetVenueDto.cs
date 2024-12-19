f27ab42672ceafe8398e28067d620e7e43d5ff66
Ali <adadashipour@gmail.com>
2024-12-19 20:54:32 +0000

    Display full venue names instead of acronyms for better UX

--- ThAmCo.Events/Dtos/GetVenueDto.cs
+++ ThAmCo.Events/Dtos/GetVenueDto.cs
@@ -11,6 +11,31 @@ namespace ThAmCo.Events.Dtos
 
         public string VenueCode { get; set; }
 
+        public string VenueName => VenueCode switch
+        {
+            "BLKTW" => "Blackthorn Workshop",
+            "BRCHH" => "Birch Hall",
+            "CMPGR" => "Competition Grounds",
+            "CRKHL" => "Crackling Hall",
+            "ELMRG" => "Elm Ridge Gardens",
+            "FDLCK" => "The Fiddler's Cockatoo",
+            "GLTNK" => "Glisten Tank",
+            "HMSCR" => "HomeScape Cradle",
+            "HNTWM" => "Huntwick Manor",
+            "LNKRY" => "The Linkery",
+            "MNSYN" => "Moonshine Inn",
+            "RIVPT" => "Riverpoint Terrace",
+            "SLTCR" => "Saltmarsh Retreat",
+            "SPHRD" => "Sapphire Docks",
+            "STGHM" => "Stargaze Hall",
+            "STPFL" => "Stepfield Hall",
+            "SUNPL" => "Sunset Pavilion",
+            "TNDMR" => "Tinder Manor",
+            "WDRSC" => "Woodrose Castle",
+            "WTHTR" => "Willow Theatre",
+            _ => "Unknown Venue"
+        };
+
         public DateTime WhenMade { get; set; }
 
         public string StaffId { get; set; }
