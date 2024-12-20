00c69e8e0a2bacfe1738b52fc59b833640eb68db
Ali <adadashipour@gmail.com>
2024-12-20 20:41:41 +0000

    Implement GuestVM for Guest Index page

--- ThAmCo.Events/Pages/GuestList/Index.cshtml.cs
+++ ThAmCo.Events/Pages/GuestList/Index.cshtml.cs
@@ -1,28 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ThAmCo.Events.Data;
+using ThAmCo.Events.ViewModels;
 
 namespace ThAmCo.Events.Pages.GuestList
 {
     public class IndexModel : PageModel
     {
-        private readonly ThAmCo.Events.Data.EventDbContext _context;
+        private readonly EventDbContext _context;
+        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ThAmCo.Events.Data.EventDbContext context)
+        public IndexModel(EventDbContext context, ILogger<IndexModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        public IList<Guest> Guest { get;set; } = default!;
+        public IList<GuestViewModel> Guests { get; set; } = new List<GuestViewModel>();
+
+        [TempData]
+        public string? StatusMessage { get; set; }
 
         public async Task OnGetAsync()
         {
-            Guest = await _context.Guests.ToListAsync();
+            var guests = await _context.Guests
+                .Include(g => g.GuestBookings)
+                .ThenInclude(gb => gb.Event)
+                .ToListAsync();
+
+            Guests = guests.Select(g => new GuestViewModel
+            {
+                GuestId = g.GuestId,
+                Name = g.Name,
+                Email = g.Email,
+                Phone = g.Phone,
+                Bookings = g.GuestBookings.Select(gb => new GuestViewModel.GuestBookingSummary
+                {
+                    EventId = gb.EventId,
+                    EventTitle = gb.Event?.Title ?? "Unknown Event",
+                    EventDate = gb.Event?.Date ?? DateTime.MinValue,
+                    HasAttended = gb.HasAttended,
+                    IsCancelled = gb.IsCancelled
+                }).ToList()
+            }).ToList();
         }
     }
 }
