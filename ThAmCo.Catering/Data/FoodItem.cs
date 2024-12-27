e8ade57396e00866d4e8d1da4883eb1380114f6a
DrEpico <76069708+DrEpico@users.noreply.github.com>
2024-12-27 18:25:40 +0000

    Minor refactor

--- ThAmCo.Catering/Data/FoodItem.cs
+++ ThAmCo.Catering/Data/FoodItem.cs
@@ -3,27 +3,27 @@
     public class FoodItem
     {
         public int FoodItemId { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public float UnitPrice { get; set; }
 
         public List<MenuFoodItem>? MenuFoodItems { get; set; }
 
-        public FoodItem()
-        {
-            Description = string.Empty;
-        }
+        //public FoodItem()
+        //{
+            
+        //}
 
-        public FoodItem(int foodItemId, string description, float unitPrice)
-        {
-            FoodItemId = foodItemId;
-            Description = description;
-            UnitPrice = unitPrice;
-        }
+        //public FoodItem(int foodItemId, string description, float unitPrice)
+        //{
+        //    FoodItemId = foodItemId;
+        //    Description = description;
+        //    UnitPrice = unitPrice;
+        //}
 
-        public FoodItem(string description, float unitPrice)
-        {
-            Description = description;
-            UnitPrice = unitPrice;
-        }
+        //public FoodItem(string description, float unitPrice)
+        //{
+        //    Description = description;
+        //    UnitPrice = unitPrice;
+        //}
     }
 }
