2519a0a868405112988f5d8d2d8b105cae68966d
DrEpico <76069708+DrEpico@users.noreply.github.com>
2024-12-27 19:10:23 +0000

    Add comments

--- Tests/ActionResultExtensions.cs
+++ Tests/ActionResultExtensions.cs
@@ -9,6 +9,13 @@ namespace Tests
 {
     public static class ActionResultExtensions
     {
+        /// <summary>
+        /// This custom extention method checks if the result is a success status code. Simulating EnsureSuccessCode() 
+        /// method like behaviour as ASP.NET Core or the testing framework has no built in method for directly validating 
+        /// success status codes in IActionResult.
+        /// </summary>
+        /// <param name="result">The returned object from an API</param>
+        /// <exception cref="Xunit.Sdk.XunitException"></exception>
         public static void EnsureSuccessStatusCode(this IActionResult result)
         {
             if (result is ObjectResult objectResult)
--- Tests/UnitTestDemo.cs
+++ Tests/UnitTestDemo.cs
@@ -10,14 +10,26 @@ using Microsoft.AspNetCore.Builder;
 
 namespace Tests
 {
+    /// <summary>
+    /// This class contains demo unit tests for the MenuItemsController class.
+    /// I have demonstrated how to write unit tests for the GetFoodItems method.
+    /// Note: I figured out how I can write unit tests too late in the project but otherwise I would have written tests while
+    /// development, which would've been a great way to proceed with the testing criteria of the assignment.
+    /// Note 2: These unit tests mostly only look for a success status code, which is meant to simulates black-box testing.
+    /// </summary>
     public class MenuItemsControllerTests
     {
+        /// <summary>
+        /// This unit test checks if the GetFoodItems method returns an OK result.
+        /// </summary>
         [Fact]
         public async Task AddFoodItem_ShouldReturnCreatedResult()
         {
+            // Arrange
             var context = new CateringDbContext();
             var controller = new FoodItemsController(context);
 
+            // Act
             var result = await controller.AddFoodItem(new FoodItemDto { Description = "Test Food Item", UnitPrice = 10 });
 
             // Assert
@@ -39,8 +51,7 @@ namespace Tests
             var result = await controller.EditFoodItem(13, new FoodItemDto { Description = "Editing Food Item", UnitPrice = 10 });
 
             // Assert
-            result.EnsureSuccessStatusCode(); // Verify the result has a success status code
+            result.EnsureSuccessStatusCode(); // Verify the result has a success status code using the custom extension method.
         }
-
     }
 }
