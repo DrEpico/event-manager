0cb9ca7494c4c33919612731d5a7e4c404433f6c
DrEpico <76069708+DrEpico@users.noreply.github.com>
2025-01-04 21:24:08 +0000

    Add display attribute to the required properties such as EventType -> Event Type

--- ThAmCo.Events/Data/Event.cs
+++ ThAmCo.Events/Data/Event.cs
@@ -5,13 +5,20 @@ namespace ThAmCo.Events.Data
     public class Event
     {
         public int EventId { get; set; }
+        [Display(Name = "Event Title", Description = "The title of the event", Prompt = "Enter the event title")]
         public string Title { get; set; }
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
         //[DataType(DataType.Time)]
+        [Display(Name = "Stard Time")]
         public TimeSpan StartTime { get; set; }
+
         //[DataType(DataType.Time)]
+        [Display(Name = "End Time")]
         public TimeSpan EndTime { get; set; }
+
+        [Display(Name = "Event Type")]
         public string EventType { get; set; } = string.Empty;
 
         // This is a computed property that will return the full event type name
--- ThAmCo.Events/Data/GuestBooking.cs
+++ ThAmCo.Events/Data/GuestBooking.cs
@@ -1,11 +1,17 @@
-﻿namespace ThAmCo.Events.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThAmCo.Events.Data
 {
     public class GuestBooking
     {
         public int GuestBookingId { get; set; }
         public int GuestId { get; set; }
         public int EventId { get; set; }
+
+        [Display(Name = "Attended")]
         public bool HasAttended { get; set; }
+
+        [Display(Name = "Cancelled")]
         public bool IsCancelled { get; set; }
 
         // Navigation Properties
