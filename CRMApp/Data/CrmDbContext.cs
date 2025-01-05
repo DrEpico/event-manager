6db8082601b0ea1ed52a7ba7a83f1e31edd36639
DrEpico <76069708+DrEpico@users.noreply.github.com>
2025-01-05 18:39:52 +0000

    Create DB context and apply migrations

---
+++ CRMApp/Data/CrmDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using CRMApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CRMApp.Data
+{
+    public class CrmDbContext : IdentityDbContext
+    {
+        public DbSet<ManagerEntity> Managers { get; set; }
+        public string DbPath { get; set; } = string.Empty;
+
+        public CrmDbContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "ThAmCo.CRM.db");
+            //Console.WriteLine($"Database Path: {DbPath}"); //Debugging
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
+
+
+        
